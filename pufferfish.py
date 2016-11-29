3fe469cf9416fcb36e212cb4237e81bab4c106db
Chief Keef <pcross2@lion.lmu.edu>
2016-11-28 21:36:36 -0800

    IDK

--- pufferfish.py
+++ pufferfish.py
@@ -1,6 +1,11 @@
-def main(board):
+import chess
+def greeting():
 	name = "Pufferfish"
 	description = "Dankest fish in the sea!"
+	return ("Hello I am %s, the %s" % (name,description))
+
+def main():
+	print(greeting())
 
 
 
